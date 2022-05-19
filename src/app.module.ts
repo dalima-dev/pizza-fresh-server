562d2836a183771d4a721e311f575f228851098b
Daniel Alves <dalima.dev@gmail.com>
2022-05-18 23:12:13 -0300

    feat

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TableModule } from './Table/table.module';
+import { TableModule } from './table/table.module';
 
 @Module({
   imports: [TableModule],
