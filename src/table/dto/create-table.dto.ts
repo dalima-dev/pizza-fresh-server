476323607f2216141e74c1071511ea8dd5607d49
Daniel Alves <dalima.dev@gmail.com>
2022-05-18 23:13:10 -0300

    feat

--- src/table/dto/create-table.dto.ts
+++ src/table/dto/create-table.dto.ts
@@ -1,9 +1,12 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsNumber, IsPositive } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsPositive } from 'class-validator';
 
-export class CreateTableDto{
-    @IsNumber()
-    @IsPositive()
-    @ApiProperty()
-    number: number;
+export class CreateTableDto {
+  @IsNumber()
+  @IsPositive()
+  @ApiProperty({
+    description: 'O numero da mesa',
+    example: 1,
+  })
+  number: number;
 }
