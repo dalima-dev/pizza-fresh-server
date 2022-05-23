ee275fc2f060e1ad7574c4e2ad6ecdcc6a2c41dc
Daniel Alves <dalima.dev@gmail.com>
2022-05-23 15:47:39 -0300

    feeat(product): generate resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TableModule } from './table/table.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [TableModule, PrismaModule],
+  imports: [PrismaModule, TableModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
