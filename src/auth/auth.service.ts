a4abe63d277ebdea4aa1a5d4ea8ce0f2af58f2b4
Daniel Alves <dalima.dev@gmail.com>
2022-05-25 19:44:34 -0300

    feat(auth): implement bcrypt in login method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { LoginResponseDto } from './dto/login-response.dto';
 import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly prisma: PrismaService) {}
+
   async login(loginDto: LoginDto): Promise<LoginResponseDto> {
+    const { nickname, password } = loginDto;
+
+    const user = await this.prisma.user.findUnique({ where: { nickname } });
+    if (!user) throw new UnauthorizedException('Usuario e/ou senha invalidos');
+
+    const isHashValid = await bcrypt.compare(password, user.password);
+    if (!isHashValid)
+      throw new UnauthorizedException('Usuario e/ou senha invalidos');
+
+    delete user.password;
 
     return {
-      token: 'fdsafads', 
-      user: undefined
-    }
+      token: 'Teste',
+      user,
+    };
   }
 }
