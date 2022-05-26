6984e0b200b2fb0b8f05132c7d35ec1e183954b8
Daniel Alves <dalima.dev@gmail.com>
2022-05-26 00:02:39 -0300

    feat: add guard to all endpoints

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    Passport Module.register({
+      defaultStrategy: 'jwt',
+    }),
+  ],
   controllers: [OrderController],
   providers: [OrderService],
 })
