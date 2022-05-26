ee960c62b76ebb156392d33561979acee62f9a29
Daniel Alves <dalima.dev@gmail.com>
2022-05-26 10:36:20 -0300

    chore(app): improve status endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getAppStatus(): string {
-    return 'Server is running! 🚀\n Please check http://localhost:3000/api for Swagger docs...';
+  getAppStatus(baseUrl: string) {
+    return {
+      status: `Server is running! 🚀\n Please check ${baseUrl}/api for Swagger docs...`,
+      docs: baseUrl + '/api',
+    };
   }
 }
